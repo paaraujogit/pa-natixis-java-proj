81b2e1b1fda3e5cc59ebdb241120f1c92d42c8e5
inesSimoes <34243783+inesSimoes@users.noreply.github.com>
2017-12-05 12:01:37 +0000

    Operacao 5

---
+++ src/pa/calc/Oper05.java
@@ -0,0 +1,19 @@
+package pa.calc;
+
+public class Oper05 implements ItfOper{
+
+	@Override
+	public double oper(double op1, double op2) {
+		double value=0.0;
+		if(op2>10.0) {
+		int op2_int= (int) op2;
+		
+			for(int i=0; i<op2_int;i++) {
+				value=i+op1;
+		}
+		}else {
+			value = Math.min(op1,op2); 
+		}
+		return (value);
+	}
+}
