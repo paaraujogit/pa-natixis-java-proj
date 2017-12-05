5e56a1d9b469e1ba4f53818ea6305a4eb8c7c820
nunomachado <nuno.fidalgo.machado@gmail.com>
2017-12-05 15:34:45 +0000

    "method refactor oper"

--- src/pa/calc/Oper07.java
+++ src/pa/calc/Oper07.java
@@ -5,7 +5,7 @@ public class Oper07 implements ItfOper {
 	@Override
 	public double oper(double op1, double op2) {
 		
-		double calc = ((op1*op2) * (op1+op2) * (op2/op1))/2;
+		double calc = ((op1*op2) * (op2*op2) * (op2/op1))/2;
 		
 		return calc;
 	}
