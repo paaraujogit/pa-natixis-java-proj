4dddedbaaa9f7cd3ddf623ed001e7ff05dbc0cfb
nunomachado <nuno.fidalgo.machado@gmail.com>
2017-12-05 11:52:07 +0000

    added operation 7 to the repository

---
+++ src/pa/calc/Oper07.java
@@ -0,0 +1,13 @@
+package pa.calc;
+
+public class Oper07 implements ItfOper {
+
+	@Override
+	public double oper(double op1, double op2) {
+		
+		double calc = ((op1*op2) * (op1+op2) * (op2/op1))/2;
+		
+		return calc;
+	}
+	
+}
