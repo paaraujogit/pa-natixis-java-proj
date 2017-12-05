9d46cdb67a5ba75f961f11967faa432fc3448449
Léo <leonardo.pinheiro@ua.pt>
2017-12-05 15:34:16 +0000

    cenas #36

--- src/pa/calc/Oper06.java
+++ src/pa/calc/Oper06.java
@@ -4,7 +4,7 @@ public class Oper06 implements ItfOper{
 
 	@Override
 	public double oper(double op1, double op2) {
-		System.out.println("#36");
+		System.out.println(" #36 ");
 		return op1*op1 - 2*(op1+op2) - op2*op2;
 	}
 
