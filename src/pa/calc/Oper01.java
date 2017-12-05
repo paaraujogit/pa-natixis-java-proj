f2ddd2b9ab4f17d149e17b87e4ec53bc06b2de5d
Alexandre Marques <alexandre.morais.marques@hotmail.com>
2017-12-05 14:52:13 +0000

    Imprime resultado da Hipotenusa na Consola

--- src/pa/calc/Oper01.java
+++ src/pa/calc/Oper01.java
@@ -3,10 +3,11 @@ package pa.calc;
 import static java.lang.Math.sqrt;
 
 public class Oper01 implements ItfOper {
-
+    //Calcular hipotenusa
 	@Override
 	public double oper(double op1, double op2) {
-		return sqrt(op1*op1 + op2*op2);
+    	double hipo = sqrt(op1*op1 + op2*op2);
+		System.out.println(hipo);
+		return hipo;	
 	}
-
 }
