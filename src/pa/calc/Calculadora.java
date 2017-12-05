1d3f11f882abf179c751a004658b446b86656b60
paaraujogit <paaraujo@gmail.com>
2017-12-05 11:16:31 +0000

    Initial Calculator

---
+++ src/pa/calc/Calculadora.java
@@ -0,0 +1,75 @@
+package pa.calc;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// OperEx Example
+		OperEx opex=new OperEx();
+		double resEx = opex.oper(1, 2);
+		System.out.println(resEx);
+		
+		// ToDo - All Students Must Implement one Operation
+		
+		// - Oper01 - Alexandre M.
+		
+//		Oper01 ope01=new Oper01();
+//		double res01 = opex.oper(1, 2);
+//		System.out.println(res01);
+
+		// - Oper02 - Alexandre T.
+		
+//		Oper02 ope02=new Oper01();
+//		double res02 = opex.oper(1, 2);
+//		System.out.println(res02);
+
+		// - Oper03 - António F.
+		
+//		Oper03 ope03=new Oper01();
+//		double res03 = opex.oper(1, 2);
+//		System.out.println(res03);
+
+		// - Oper04 - Gonçalo F.
+		
+//		Oper04 ope04=new Oper01();
+//		double res04 = opex.oper(1, 2);
+//		System.out.println(res04);
+
+		// - Oper05 - Inês S.
+		
+//		Oper05 ope05=new Oper01();
+//		double res05 = opex.oper(1, 2);
+//		System.out.println(res05);
+
+		// - Oper06 - Leonardo P.
+		
+//		Oper06 ope06=new Oper01();
+//		double res06 = opex.oper(1, 2);
+//		System.out.println(res06);
+
+		// - Oper07 - Nuno M.
+		
+//		Oper07 ope07new Oper01();
+//		double res07 = opex.oper(1, 2);
+//		System.out.println(res07);
+
+		// - Oper08 - Pedro B.
+		
+//		Oper08 ope08=new Oper01();
+//		double res08 = opex.oper(1, 2);
+//		System.out.println(res08);
+
+		// - Oper09 - Rui L.
+		
+//		Oper09 ope09=new Oper01();
+//		double res09 = opex.oper(1, 2);
+//		System.out.println(res09);
+
+		// - Oper10 - Rui M.
+		
+//		Oper10 ope10=new Oper10();
+//		double res10 = opex.oper(1, 2);
+//		System.out.println(res01);
+
+	}
+
+}
