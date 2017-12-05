35102e4a7f1885c0693ce2a875fa447b9ac936d4
Léo <leonardo.pinheiro@ua.pt>
2017-12-05 14:21:48 +0000

    correção de operação

--- src/pa/calc/Oper06.java
+++ src/pa/calc/Oper06.java
@@ -5,7 +5,7 @@ public class Oper06 implements ItfOper{
 	@Override
 	public double oper(double op1, double op2) {
 		
-		return op1*op1 + 2*(op1+op2) + op2*op2;
+		return op1*op1 - 2*(op1+op2) + op2*op2;
 	}
 
 }
