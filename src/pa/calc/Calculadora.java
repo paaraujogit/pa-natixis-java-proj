853c4700d6a16f217d0654c2a1ea1380ed26d207
nunomachado <nuno.fidalgo.machado@gmail.com>
2017-12-05 12:00:02 +0000

    added operation 7

--- src/pa/calc/Calculadora.java
+++ src/pa/calc/Calculadora.java
@@ -18,49 +18,49 @@ public class Calculadora {
 
 		// - Oper02 - Alexandre T.
 		
-//		Oper02 ope02=new Oper01();
+//		Oper02 ope02=new Oper02();
 //		double res02 = opex.oper(1, 2);
 //		System.out.println(res02);
 
-		// - Oper03 - António F.
+		// - Oper03 - Antï¿½nio F.
 		
-//		Oper03 ope03=new Oper01();
+//		Oper03 ope03=new Oper03();
 //		double res03 = opex.oper(1, 2);
 //		System.out.println(res03);
 
-		// - Oper04 - Gonçalo F.
+		// - Oper04 - Gonï¿½alo F.
 		
-//		Oper04 ope04=new Oper01();
+//		Oper04 ope04=new Oper04();
 //		double res04 = opex.oper(1, 2);
 //		System.out.println(res04);
 
-		// - Oper05 - Inês S.
+		// - Oper05 - Inï¿½s S.
 		
-//		Oper05 ope05=new Oper01();
+//		Oper05 ope05=new Oper05();
 //		double res05 = opex.oper(1, 2);
 //		System.out.println(res05);
 
 		// - Oper06 - Leonardo P.
 		
-//		Oper06 ope06=new Oper01();
+//		Oper06 ope06=new Oper06();
 //		double res06 = opex.oper(1, 2);
 //		System.out.println(res06);
 
 		// - Oper07 - Nuno M.
 		
-//		Oper07 ope07new Oper01();
-//		double res07 = opex.oper(1, 2);
+//		Oper07 ope07 = new Oper07();
+//		double res07 = ope07.oper(1, 2);
 //		System.out.println(res07);
 
 		// - Oper08 - Pedro B.
 		
-//		Oper08 ope08=new Oper01();
+//		Oper08 ope08=new Oper08();
 //		double res08 = opex.oper(1, 2);
 //		System.out.println(res08);
 
 		// - Oper09 - Rui L.
 		
-//		Oper09 ope09=new Oper01();
+//		Oper09 ope09=new Oper09();
 //		double res09 = opex.oper(1, 2);
 //		System.out.println(res09);
 
